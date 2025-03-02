6d51a694cbcb9d5fe048dbd940798607dfb149ae
Kumar Vijay <vijay81238@gmail.com>
2025-03-02 13:57:17 +0530

    updated jwtutil

--- springboot-with-openapi/src/main/java/com/example/vijay/util/JwtUtil.java
+++ springboot-with-openapi/src/main/java/com/example/vijay/util/JwtUtil.java
@@ -4,13 +4,15 @@ package com.example.vijay.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private static final String SECRET_KEY = "mySecretKey";
+    private static final SecretKey SECRET_KEY = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
     private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
 
     public String generateToken(String username) {
